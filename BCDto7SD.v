54d88e7e67bba695a868d516ea264c0b8e59fdf4
Griffin O'Connor <griffinjoconnor@gmail.com>
2026-09-14 10:29:23 -0400

    new

--- BCDto7SD.v
+++ BCDto7SD.v
@@ -2,11 +2,13 @@ module BCDto7SD (
     input [3:0] in;
     output [6:0] Seg;
 );
-//Case 
+
+//Enable anode corresponding to rightmost 7SD digit
+assign an = 4'b1110;
 
 always @(*) begin
     case(in)
-        //Input cases 0-9, assign corresponding 7SD output
+        //Input cases 0-F, assign corresponding 7SD output
         4'b0000: seg = 7'b0000001;
         4'b0001: seg = 7'b1001111;
         4'b0010: seg = 7'b0010010;
@@ -17,6 +19,12 @@ always @(*) begin
         4'b0111: seg = 7'b0001111;
         4'b1000: seg = 7'b0000000;
         4'b1001: seg = 7'b0000100;
+        4'b1010: seg = 7'b0001000; 
+        4'b1011: seg = 7'b1100000; 
+        4'b1100: seg = 7'b0110001; 
+        4'b1101: seg = 7'b1000010; 
+        4'b1110: seg = 7'b0110000; 
+        4'b1111: seg = 7'b0111000; 
         default: seg = 7'b1111111; 
 
     endcase
